f958d1bc1a902b41feac9eae44457979775d07e2
a00880208 <alam160@my.bcit.ca>
2017-10-15 00:37:09 -0700

    Moved filesize into FileObj, added file extension

--- src/main/java/fileExplorerService/FileObj.java
+++ src/main/java/fileExplorerService/FileObj.java
@@ -1,8 +1,12 @@
 package fileExplorerService;
 
+
 public class FileObj {
+    final long kbytes = 1024;
 
 	String fileName;
+
+	String fileExtension;
 	
 	float fileSize;
 	
@@ -10,6 +14,23 @@ public class FileObj {
 	public FileObj(String fileName, float fileSize){
 		this.fileName = fileName;
 		this.fileSize = fileSize;
+		this.fileExtension = this.getFileExtension(fileName);
 	}
-	
+
+	private String getFileExtension(String fileName){
+		String[] fileNameArray =fileName.split("\\.");
+
+		String fileExtension = fileNameArray[fileNameArray.length - 1];
+
+		return fileExtension;
+	}
+
+    public float getFileSizeInMB(){
+
+        // Get length of file in bytes
+        float fileSizeInKB = fileSize / this.kbytes;
+        float fileSizeInMB = fileSizeInKB / this.kbytes;
+
+        return fileSizeInMB;
+    }
 }
